9956c703328fbe66fc0df5c11876a40529d3ba26
martin-king-b786 <m.king@brand786.com>
2014-07-31 15:17:46 +0100

    Emergency: - Hover over emergency removes image filter - Read Feedback spacing - Mobile view sorted

--- js/emergency.js
+++ js/emergency.js
@@ -11,6 +11,21 @@ function emFeatHeight () {
     
 }
 
+$(document).ready(function(){
+    $('.emergency-section .mobile-title').click(function(){
+        $(this).parent().children('.mobile-container').slideToggle();
+        $(this).children('img').toggleClass('rotate').toggleClass('active');
+        if($(this).children('img').hasClass('active')){
+            console.log('fdgdf');
+            $(this).children('img').attr('src','./css/images/arrow_hollow.png');
+        }
+        else {
+            $(this).children('img').attr('src','./css/images/arrow_colour.png');
+        }
+        
+        
+    });
+});
 $(document).ready(emFeatHeight);
 $(window).load(emFeatHeight);
 $(window).resize(emFeatHeight);
