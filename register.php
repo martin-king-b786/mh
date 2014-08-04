4c7a8c4c2fa5966e35aa4e61af4a9b299c91a702
martin-king-b786 <m.king@brand786.com>
2014-08-04 12:55:02 +0100

    Assorted Changes + World Map added

---
+++ register.php
@@ -0,0 +1,438 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Muslim Hands</title>
+	<meta name="description" content="Official website of Muslim Hands" />
+	<meta name="keywords" content="muslim, hands" />
+
+	<link rel="stylesheet" type="text/css" media="all" href="css/bootstrap.min.css">
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" type="text/css" media="all" href="./css/style.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="./css/mediastyle.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="./css/jcarousel.responsive.css" />
+
+
+
+
+	<link rel="apple-touch-icon-precomposed" sizes="60x60" href="./favicon/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="./favicon/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="./favicon/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="./favicon/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="./favicon/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/png" href="./favicon/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="./favicon/favicon-16x16.png" sizes="16x16" />
+	<link rel="icon" href="./favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="./js/scripts-main.js"></script>
+	<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="./js/jquery.jcarousel.min.js"></script>
+	<script type="text/javascript" src="./js/jcarousel.responsive.js"></script>
+	<script type="text/javascript" src="./js/jquery.matchHeight-min.js"></script>
+
+	<link rel="stylesheet" type="text/css" media="all" href="./css/register.css" />
+
+	<!--<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', '', '');
+		ga('require', 'displayfeatures');
+		ga('send', 'pageview');
+	</script>-->
+         <script type="text/javascript">
+            var RecaptchaOptions = {
+               theme : 'clean'
+            };
+        </script>
+</head>
+
+<body>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=185021011509911&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+<div id="container">
+
+<section id="header">
+	<a id="logo" href="./"><img src="./css/images/logo.png" alt="Muslim Hands" /></a>
+
+	<nav id="nav">
+		<ul id="navleft">
+			<li><a href="#">Home</a></li>
+			<li>
+				<a href="#" class="navarr">Appeals</a>
+					<div id="navappcon" class="subnav">
+						<div id="navappeal">
+							<img src="./css/images/icon-appeal.png" alt="" />
+							<div id="navappealtext">
+								<h3>Ramadan 2014</h3>
+								<p>Support our ongoing projects in Ramadan</p>
+							</div>
+						</div>
+						<ul class="col2 more">
+							<li><a href="#"><span class="icon iraq">Iraq Emergency</a></li>
+							<li><a href="#"><span class="icon syria">Syria Emergency</a></li>
+							<li><a href="#"><span class="icon car">CAR Emergency</a></li>
+							<li><a href="#"><span class="icon food">Food 4 Kids</a></li>
+						</ul>
+					</div>
+			</li>
+			<li>
+				<a href="#" class="navarr">Our Work</a>
+					<ul class="col2 more subnav">
+						<li><a href="#"><span class="icon areas">Areas of Work</a></li>
+						<li><a href="#"><span class="icon emergencies">Emergencies</a></li>
+						<li><a href="#"><span class="icon campaign">Campaign</a></li>
+						<li><a href="#"><span class="icon where">Where We Work</a></li>
+					</ul>
+			</li>
+			<li>
+				<a href="#" class="navarr">Get Involved</a>
+					<ul class="col2 more subnav">
+						<li><a href="#"><span class="icon volunteer">Volunteer</a></li>
+						<li><a href="#"><span class="icon events">Events</a></li>
+						<li><a href="#"><span class="icon workwith">Work With Us</a></li>
+						<li><a href="#"><span class="icon publications">Our Publications</a></li>
+					</ul>
+			</li>
+			<li>
+				<a href="#" class="navarr">About Us</a>
+					<ul class="col2 subnav">
+						<li><a href="#"><span class="icon contact">Contact Us</a></li>
+						<li><a href="#"><span class="icon find">Vacancies</a></li>
+					</ul>
+			</li>
+		</ul>
+
+		<ul id="navright">
+			<li id="lidonate">
+				<a href="#" id="donate" class="navarr">Donate</a>
+					<ul id="donatenav" class="subnav">
+						<li><a href="#">Donation</a></li>
+						<li><a href="#">Zakat</a></li>
+						<li><a href="#">Orphans</a></li>
+						<li><a href="#">Great Charity Gifts</a></li>
+					</ul>
+			</li>
+			<li>
+				<a href="#" id="basketnav" class="navbtn"><span>2</span><img src="./css/images/basket.png" alt="" /></a>
+				<div id="basketdetail" class="subnav">
+					<div class="relative">
+						<span class="uparr"></span>
+						<p class="basketamount">&pound;50</p>
+						<h3>My Basket</h3>
+						<p>Two Items</p>
+					</div>
+				</div>
+			</li>
+			<li>
+				<a href="#" id="searchnav" class="navbtn"><img src="./css/images/search.png" alt="" /></a>
+				<div id="searchdetail" class="subnav">
+					<div class="relative">
+						<span class="uparr"></span>
+						<form method="post" action="">
+							<input type="text" name="s" id="s" placeholder="Search here" />
+							<input type="submit" value="Subscribe" />
+						</form>
+					</div>
+				</div>
+			</li>
+			<li id="limnav">
+				<a href="#" id="mnav" class="navbtn"></a>
+				<ul id="mobilenav" class="subnav">
+					<div class="relative">
+						<span class="uparr"></span>
+
+					<li><a href="#">Home</a></li>
+					<li>
+						<a href="#" class="navarr1">Appeals</a>
+							<ul class="subsubnav">
+								<li><a href="#"><span class="icon iraq">Iraq Emergency</a></li>
+								<li><a href="#"><span class="icon syria">Syria Emergency</a></li>
+								<li><a href="#"><span class="icon car">CAR Emergency</a></li>
+								<li><a href="#"><span class="icon food">Food 4 Kids</a></li>
+							</ul>
+						</li>
+					<li>
+						<a href="#" class="navarr1">Our Work</a>
+							<ul class="subsubnav">
+								<li><a href="#"><span class="icon areas">Areas of Work</a></li>
+								<li><a href="#"><span class="icon emergencies">Emergencies</a></li>
+								<li><a href="#"><span class="icon campaign">Campaign</a></li>
+								<li><a href="#"><span class="icon where">Where We Work</a></li>
+							</ul>
+					</li>
+					<li>
+						<a href="#" class="navarr1">Get Involved</a>
+							<ul class="subsubnav">
+								<li><a href="#"><span class="icon volunteer">Volunteer</a></li>
+								<li><a href="#"><span class="icon events">Events</a></li>
+								<li><a href="#"><span class="icon workwith">Work With Us</a></li>
+								<li><a href="#"><span class="icon publications">Our Publications</a></li>
+							</ul>
+					</li>
+					<li>
+						<a href="#" class="navarr1">About Us</a>
+							<ul class="subsubnav">
+								<li><a href="#"><span class="icon contact">Contact Us</a></li>
+								<li><a href="#"><span class="icon find">Vacancies</a></li>
+							</ul>
+					</li>
+					</div>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</section>
+
+<div id="register">
+    
+    
+    <div id="register-message">
+        <p>Please login using the form below. If you do not currently have an account please register first.</p>
+    </div>
+    
+    <section id="register-section" class="col-md-6 col-xs-12">
+        <h2>Register</h2>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Email Address</label>
+            <input class="col-sm-8 col-xs-12" type="email"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Password</label>
+            <input class="col-sm-8 col-xs-12" type="password"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Confirm Password</label>
+            <input class="col-sm-8 col-xs-12" type="password"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Title*</label>
+            <select class="col-sm-4 col-xs-12">
+                <option>Mr.</option>
+                <option>Mrs.</option>
+                <option>Miss.</option>
+                <option>Ms.</option>
+                <option>Option 5</option>
+                <option>Option 6</option>
+            </select>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">First Name*</label>
+            <input class="col-sm-8 col-xs-12" type="text"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Last Name*</label>
+            <input class="col-sm-8 col-xs-12" type="text"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Telephone*</label>
+            <input class="col-sm-8 col-xs-12" type="text"/>
+        </div>
+    </section>
+    
+    <section id="address-section" class="col-md-6 col-xs-12">
+        <h2>Your Address</h2>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Address Line 1*</label>
+            <input class="col-sm-8 col-xs-12" type="text"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Address Line 2*</label>
+            <input class="col-sm-8 col-xs-12" type="text"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Address Line 3</label>
+            <input class="col-sm-8 col-xs-12" type="text"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Town/City*</label>
+            <input class="col-sm-8 col-xs-12" type="text"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">County</label>
+            <input class="col-sm-8 col-xs-12" type="text"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Postcode*</label>
+            <input class="col-sm-8 col-xs-12" type="text"/>
+        </div>
+        <div class="row col-xs-12">
+            <label class="col-sm-4 col-xs-12">Country</label>
+            <select class="col-sm-8 col-xs-12">
+                <option>Option 1</option>
+                <option>Option 2</option>
+                <option>Option 3</option>
+                <option>Option 4</option>
+                <option>Option 5</option>
+                <option>Option 6</option>
+                <option>Option 7</option>
+                <option>Option 8</option>
+                <option>Option 9</option>
+                <option>Option 10</option>
+                <option>Option 11</option>
+                <option>Option 12</option>
+            </select>
+        </div>
+    </section>
+    
+    <section id="giftaid-section" class="col-md-12 col-xs-12">
+        <h2>Gift Aid</h2>
+        <h3>Gift Aid allows you to increase the value of your donation at no cost to you. You can read more about Gift Aid...</h3>
+        <div class="row col-xs-12">
+            <input class="col-sm-4 col-xs-12" type="checkbox" name='ga1' id="ga1" />
+            <label class="col-sm-8 col-xs-12" for='ga1'><span></span>Yes, I am a UK taxpayer and would like Gift Aid claimed on my donations.</label>
+        </div>
+        <div class="row col-xs-12">
+            <input class="col-sm-4 col-xs-12" type="checkbox" name='ga2' id="ga2"/>
+            <label class="col-sm-8 col-xs-12" for='ga2'><span></span>No, I am a UK taxpayer and do not want Gift Aid claimed on my donations.</label>
+        </div>
+        <div class="row col-xs-12">
+            <input class="col-sm-4 col-xs-12" type="checkbox" name='ga3' id="ga3"/>
+            <label class="col-sm-8 col-xs-12" for='ga3'><span></span>I am not sure, please don't claim Gift Aid at the moment.</label>
+        </div>
+    </section>
+    
+    <section id="preference-section" class="col-md-12 col-xs-12">
+        <h2>Preferences</h2>
+        <div class="row col-xs-12">
+            <input class="col-sm-4 col-xs-12" type="checkbox" name='p1' id="p1"/>
+            <label class="col-sm-8 col-xs-12" for='p1'><span></span>Tick if you would like to receive a receipt via post also</label>
+        </div>
+        <div class="row col-xs-12">
+            <input class="col-sm-4 col-xs-12" type="checkbox" name='p2' id="p2"/>
+            <label class="col-sm-8 col-xs-12" for='p2'><span></span>Keep me updated via email about Muslim Hands</label>
+        </div>
+        <div class="row col-xs-12">
+            <input class="col-sm-4 col-xs-12" type="checkbox" name='p3' id="p3"/>
+            <label class="col-sm-12 col-xs-12" for='p3'><span></span>Please tick to confirm you have read and accept our terms and conditions</label>
+        </div>
+        
+    </section>
+    
+    <section id="captcha-section" class="col-md-12 col-xs-12">
+        <p>Please enter the text displayed below to confirm your registration. This is to prevent automated/spam registrations.</p>
+        <?php
+          require_once('recaptchalib.php');
+          $publickey = "6Lf15fcSAAAAAMhMsxmVuM7fptbaYNxlYoOOS1VP"; // you got this from the signup page
+          echo recaptcha_get_html($publickey);
+        ?>
+        <a class="button" href="#">Continue<img src="./css/images/arrow_hollow.png"/></a>
+        
+    </section>
+        
+</div>
+
+<section id="footer">
+	<section id="footertop">
+		<ul class="ft-donate">
+			<a href="/donate/"><h6 class="tag">Donate</h6></a>
+
+			<li><a href="/giving/zakat/">Find out about zakat</a></li>
+			<li><a href="/donate/">Make a donation</a></li>
+			<li><a href="/giving/ways-to-give/">Other ways to give</a></li>
+			<li><a href="/sponsorship/orphans/">Sponsor an orphan</a></li>
+		</ul>
+		<ul class="ft-our-work">
+			<a href="/our-work/"><h6 class="tag">Our Work</h6></a>
+
+			<li><a href="/campaigns/">Campaigns</a></li>
+			<li><a href="/emergencies/">Emergencies</a></li>
+			<li><a href="/our-work/">What we do</a></li>
+			<li><a href="/our-work/around-the-world/">Where we work</a></li>
+		</ul>
+		<ul class="ft-get-involved">
+			<a href="/get-involved/"><h6 class="tag">Get Involved</h6></a>
+
+			<li><a href="/get-involved/volunteering/">Become a volunteer</a></li>
+			<li><a href="/contact-us/">Contact us</a></li>
+			<li><a href="/publications/">Recent publications</a></li>
+			<li><a href="/events/">Upcoming events</a></li>
+		</ul>
+		<ul class="ft-about-us">
+			<a href="/about-us/"><h6 class="tag">About Us</h6></a>
+
+			<li><a href="/about-us/history/">Our history</a></li>
+			<li><a href="/about-us/partners/">Our partners</a></li>
+			<li><a href="/about-us/how-your-money-is-spent/">How your money is spent</a></li>
+			<li><a href="/about-us/vacancies/">Job vacancies</a></li>
+		</ul>
+		<ul class="ft-mh-worldwide">
+			<a href="#"><h6 class="tag">MH Worldwide</h6></a>
+
+			<li><a href="http://muslimhands.fr" target="_blank">France</a></li>
+			<li><a href="http://muslimhands.org.za" target="_blank">South Africa</a></li>
+		</ul>
+	</section>
+
+
+	<section id="footerbottom">
+		<img id="footer-logo-d" src="./css/images/footer-logo.png" alt="" />
+		<img id="footer-logo-m" src="./css/images/footer-logo-m.png" alt="" />
+		<div id="footer2">
+			<span id="mform">
+				<span id="ft-title">
+					<h6 class="tag">Stay Connected</h6>
+					<p>Join our mailing list</p>
+				</span>
+				<form id="ft-form" method="post" action="">
+					<input type="email" placeholder="your email address" />
+					<input type="submit" value="Subscribe" />
+				</form>
+			</span>
+			<span id="maccr">
+				<div class="ft-accr">
+					<a href="#"><img src="./css/images/fundraising-board.png" alt="" /></a>
+					<a href="#"><img src="./css/images/iso9001.png" alt="" /></a>
+				</div>
+				<div class="ft-social">
+					<a href="https://twitter.com/muslimhandsuk" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @muslimhandsuk</a>
+					<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+				</div>
+			</span>
+			<span id="daccr">
+				<span class="ft-social">
+					<a href="https://twitter.com/muslimhandsuk" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @muslimhandsuk</a>
+					<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+				</span>
+				<span class="ft-accr">
+					<a href="#"><img src="./css/images/fundraising-board.png" alt="" /></a>
+					<a href="#"><img src="./css/images/iso9001.png" alt="" /></a>
+				</span>
+			</span>
+		</div>
+		<div id="footerlast">
+			<span class="social">
+				<a href="#"><img src="./css/images/s-fb.png" alt="" /></a>
+				<a href="#"><img src="./css/images/s-tw.png" alt="" /></a>
+				<a href="#"><img src="./css/images/s-gp.png" alt="" /></a>
+				<a href="#"><img src="./css/images/s-yt.png" alt="" /></a>
+			</span>
+			<span class="copy">Copyright &copy; 2014 Muslim Hands. All rights reserved. 148 Gregory Boulevard, Nottingham, NG7 5JE. Charity Reg. No. 1105056. <a href="#">Privacy Policy</a>. <a href="#">Terms &amp; Conditions</a>.</span>
+		</div>
+	</section>
+</section>
+
+</div>
+
+</body>
+</html>
